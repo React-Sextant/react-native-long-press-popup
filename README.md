cb1390d716a4af5d6c6b98db2dcc98e66064e28e
罗坤 <1498710037@qq.com>
2018-11-19 21:15:37 +0800

    add example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# react-native-long-press-popup
+# React Native仿QQ/微信/iOS 长按文本弹出上下文菜单
 A React Native Popop Menu with long press open
+
+## Getting started
+
+`$ npm install react-native-long-press-popup@https://github.com/zhijiasoft/react-native-long-press-popup.git --save`
+
+## Usage
+
+```javascript
+
+```
---
+++ example/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { View,TouchableOpacity,Text } from 'react-native';
+import Popover from '../index'
+
+export default class extends React.Component {
+    render(){
+        return (
+            <View style={{flex:1}}>
+                <TouchableOpacity ref={"button"}
+                                  onLongPress={()=>{
+                                      this.refs.button.measure((ox, oy, width, height, px, py) => {
+                                          Popover.show(width,height,py,[
+                                              {
+                                                  name:'复制',
+                                                  onPress:()=>{
+                                                      alert(1);
+                                                      Popover.hide()
+                                                  }
+                                              },
+                                              {
+                                                  name:'粘贴',
+                                                  onPress:()=>{
+                                                      alert(2)
+                                                  }
+                                              },
+                                          ])
+                                      })
+                                  }}
+                >
+                    <View style={{height:50,backgroundColor:'red',justifyContent:'center',alignItems:'center'}}>
+                        <Text>onLongPress</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
